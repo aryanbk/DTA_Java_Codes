4431aca06d9c18b1c5c4932b4c42a828b51f2b0c
aryan <aryankatrodiya999@gmail.com>
2023-07-16 15:33:35 +0530

    initial commit

---
+++ encapsulationdemo/Main.java
@@ -0,0 +1,15 @@
+package com.encapsulationdemo;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Student student = new Student(10101, "Rakshit", 95, 90, 91);
+
+		Grade grade = GradeCheck.checkResult(student);
+
+		System.out.println(grade.getId() + " passed in the Exams with the Grade - " + grade.getResult());
+
+	}
+
+}
